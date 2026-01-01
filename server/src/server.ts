d226d70f34243942e7392f6a53a83825775bdf33
Breno Augusto Ferreira <brenoafsa@gmail.com>
2025-12-31 22:57:08 -0300

    Feat: implements cross origin resource sharing configuration to allow server to communicate with client

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import type { Request, Response } from 'express'
+import cors from 'cors';
 
 const app = express()
 const port = 3001
+app.use(cors({ origin: `http://localhost:3000`, credentials: true }));
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('hello world');
