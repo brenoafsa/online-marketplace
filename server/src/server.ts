8b1bad17c80a9eebb0e425e5f0bdbc5e72a6bb50
Breno Augusto Ferreira <brenoafsa@gmail.com>
2026-01-02 11:42:40 -0300

    Doc: set swagger configuration to document API routes based on a doc folder in http

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,32 @@
 import express from 'express'
 import type { Request, Response } from 'express'
 import cors from 'cors';
-import { productRouter } from './infrastructure/http/routes/product.routes';
+import { productRouter } from '@infrastructure/http/routes/product.routes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
 
 const app = express()
 const port = 3001
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Online Marketplace API',
+      version: '1.0.0',
+      description: 'API documentation for Online Marketplace',
+    },
+    servers: [
+      {
+        url: `http://localhost:${port}`,
+      },
+    ],
+  },
+  apis: ['./src/infrastructure/http/doc/*.ts'],
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(cors({ origin: `http://localhost:3000`, credentials: true }));
 app.use(express.json());
